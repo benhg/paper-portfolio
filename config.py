f17bb3f16c2525319932a41ac49ba18b2185d5c2
Ben Glick <glick@glick.cloud>
2022-01-16 19:00:16 -0800

    args

---
+++ config.py
@@ -0,0 +1,7 @@
+"""
+config.py
+
+Store configuration details here
+"""
+
+PORTFOLIO_STORAGE_DIR = "/Users/ben/portfolios"
--- paper_portfolio.py
+++ paper_portfolio.py
@@ -6,3 +6,21 @@ The `main` executable for this project.
 """
 
 import argparse
+
+PARSER_ACTION_CHOICES = ["create", "delete", "invest", "buy", "sell", "check_value", "update"]
+
+def parse_args(parser: argparse.ArgumentParser):
+	"""
+	Parse args for our program
+	"""
+	parser.add_argument("-a", "--action", help="Action to perform: Choices: {}".format('\n\t'.join(PARSER_ACTION_CHOICES)), type=str, choices=PARSER_ACTION_CHOICES, required=True)
+	parser.add_argument("-n", "--name", help="Portfolio name to act on", type=str, required=True)
+	parser.add_argument("-s", "--symbol", help="Symbol to use for action, used with buy, sell, check_value options", type=str)
+	parser.add_argument("-q", "--quantity", help="Quantity of item being bought, sold, or invested", type=str)
+	args = parser.parse_args()
+	return args
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description="View and manage paper portfolios")
+	args = parse_args(parser)
